7ea2fb8baabf10f1dcc2dec89974a6442f25c195
Bashiri Smith <bashirismith1419@gmail.com>
2021-12-21 14:12:25 -0800

    serverless function

---
+++ client/api/emailSubmit.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+export default function emailSubmit(req,res) {
+  const { email } = req.body;
+  mailchimp.setConfig({
+    apiKey: process.env.API_KEY,
+    server: "us20"
+  })
+  async function run(){
+    const response = await mailchimp.ping.get();
+    console.log(response)
+  }
+  run()
+
+  const response = await mailchimp.lists.addListMember("ac3bfabd1e", {
+      email_address: email,
+      status: "subscribed",
+    });
+    res.status(200).json(response)
+}
