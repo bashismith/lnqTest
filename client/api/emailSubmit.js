7c72209c0f784049321737ce593ad3e73990e6b0
Bashiri Smith <bashirismith1419@gmail.com>
2021-12-21 16:50:46 -0800

    async added boom

--- client/api/emailSubmit.js
+++ client/api/emailSubmit.js
@@ -1,22 +1,20 @@
 require('dotenv').config();
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
- module.exports = (req,res) => {
+ module.exports = async (req,res) => {
   const { email } = req.body;
   mailchimp.setConfig({
     apiKey: process.env.API_KEY,
     server: "us20"
   })
-  console.log('hello')
-  async function run(){
-    const response = await mailchimp.ping.get();
-    console.log(response)
+  try{
+    const response = await mailchimp.lists.addListMember("ac3bfabd1e", {
+        email_address: email,
+        status: "subscribed",
+      });
+      res.status(200).json(response)
+  }catch(err){
+    res.status(400).json(err)
   }
-  run()
 
-  const response = await mailchimp.lists.addListMember("ac3bfabd1e", {
-      email_address: email,
-      status: "subscribed",
-    });
-    res.status(200).json(response)
 }
