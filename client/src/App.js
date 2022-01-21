e71df70d321fef94d79ef51ee8e3fb4e495a4e8b
Bashiri Smith <bashirismith1419@gmail.com>
2022-01-21 12:21:08 -0800

    Adidas x sLABS

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
-import Timer from './components/Timer';
-import EmailModal from'./components/EmailModal';
 import Footer from './components/Footer';
-import logo from './images/lnq-bluecircle.png';
-import Tap from './sound/Tap.mp3';
 import Ambient from './sound/Ambient.mp3';
+import gif from './images/alpha11.gif';
 
 
 const App = () => {
-  const [showModal, setShowModal] = useState(false);
   const [clicked, setClicked] = useState(false);
+  const [word, setWord] = useState("LNQ.");
+  const [count, setCount ] = useState(0);
   const ambient = new Audio(Ambient);
   ambient.volume = 0.3;
   ambient.loop = true;
-
   const enterClick = () => {
     ambient.play();
     setClicked(!clicked)
   }
-  const openModal = () => {
-    const tapBtn = new Audio(Tap);
-    tapBtn.volume = 0.5;
-    tapBtn.play();
-    setShowModal(prev => !prev)
+
+  const cycleWords = (num) => {
+    const wordArr = [
+      'Ambient',
+      'Connected',
+      'Integrated',
+      'Engaging',
+      'Active',
+      'Inspiring',
+      'Interactive',
+      'Playful',
+      'Disruptive',
+      'Creative',
+      'Explore',
+      'Access',
+      'Ownership',
+      'Youthful',
+      'Utility',
+      'Experience',
+      'Authenticate',
+      'Exchange',
+      'Innovate',
+      'Flow',
+      'Curiosity',
+      'Impactful',
+      'Subtle',
+      'Organic',
+      'Secure',
+      'Powerful',
+      'Culture',
+      'Adidas x sLABS'
+    ];
+    if(wordArr[num]) setWord(wordArr[num])
   }
+
+  useEffect(()=> {
+    if(clicked) {
+      setTimeout(() => cycleWords(count), 500)
+      setCount((prev) => prev+=1)
+    }
+  },[word, clicked])
+
   return (
     <div>
-      {clicked ? null :
-    <div className="overlay" onClick={enterClick}>
-      <h1>Tap to Enter.</h1>
-    </div>
-      }
+      {clicked ?
       <>
-      <Navbar/>
-      {showModal ?
-        <EmailModal setShowModal={setShowModal}/>
-        :
-        <div className='timerDiv'>
-         <Timer/>
-         <div className='wearable'>
-          <span>THE WEARABLE INTERNET</span>
-         </div>
-         {clicked ? <img alt='pulseLogo' className='tap'src={logo} onClick={openModal}/> : null}
+        <Navbar/>
+        <div className='threeStripes'>
+          <h1 className='spin'>{word}</h1>
+         <img src={gif} alt='three-stripes' className='adidas' />
         </div>
-      }
-      {clicked ?<Footer/> : null }
+        <Footer/>
       </>
+      :
+      <div className="overlay" onClick={enterClick}>
+        <h1>Tap to Enter. </h1>
+      </div>
+      }
     </div>
   );
 }
