2a59170296eaecee17c830065d9a6072b075bd70
Bashiri Smith <bashirismith1419@gmail.com>
2022-05-21 15:26:12 -0700

    media query

--- client/src/components/InterviewVideo.js
+++ client/src/components/InterviewVideo.js
@@ -5,7 +5,6 @@ const InterviewVideo = () => {
     <div className="modal">
       <div className="content">
       <iframe width="600" height="400" src="https://vimeo.com/event/2140910/embed/049a160997" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      {/* <iframe src="https://vimeo.com/event/2140878/embed" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe> */}
       </div>
     </div>
   )
